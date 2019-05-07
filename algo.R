7bcc22b59ba2f3bd4ce5c33b74fae49dfa7aa300
Julius-V <julius.vainora@gmail.com>
2019-05-07 21:26:47 +0200

    Starting to implemetent Ordered HyperCubes

--- algo.R
+++ algo.R
@@ -1,5 +1,20 @@
 rm(list = ls())
-partition <- function(D, L = NULL, C) {
+xOHC <- function(X, C) {
+  
+}
+partOHC <- function(Y, X, L, C) {
+  n <- nrow(X)
+  thrs <- sort(Y)[floor(n / L * (1:L))]
+  nums0 <- as.numeric(cut(Y, c(-Inf, thrs), labels = 1:L, include.lowest = TRUE))
+  maxs <- apply(X, 1, max)
+  subX <- lapply(split(X, nums0), matrix, ncol = ncol(X))
+  nums <- numeric(n)
+  for(i in 1:L)
+    nums[nums0 == i] <- xOHC()
+  subX <- lapply(subX, xOHC, C)
+}
+
+partSEB <- function(D, L = NULL, C) {
   nr <- nrow(D)
   nc <- ncol(D)
   col <- D[, 1]
@@ -10,7 +25,7 @@ partition <- function(D, L = NULL, C) {
     ms <- lapply(split(D[, -1, drop = FALSE], nums0), matrix, ncol = nc - 1)
     nums <- numeric(nr)
     for(i in 1:nints)
-      nums[nums0 == i] <- partition(ms[[i]], C = C) + C^(nc - 1) * (i - 1)
+      nums[nums0 == i] <- partSEB(ms[[i]], C = C) + C^(nc - 1) * (i - 1)
     nums
   } else {
     nums0
@@ -19,9 +34,9 @@ partition <- function(D, L = NULL, C) {
 nc <- 3
 L <- 5
 C <- 3
-D <- matrix(c(runif(100000), rnorm(100000), rlnorm(100000, sdlog = 0.5)), ncol = nc)
-# D <- matrix(rnorm(50000 * nc), ncol = nc)
-table(partition(D, L, C))
+# D <- matrix(c(runif(100000), rnorm(100000), rlnorm(100000, sdlog = 0.5)), ncol = nc)
+D <- matrix(runif(50000 * nc), ncol = nc)
+table(partSEB(D, L, C))
 L * C^(nc - 1)
 
 library(RColorBrewer)
@@ -29,6 +44,6 @@ n <- L * C^(nc - 1)
 qual_col_pals <- brewer.pal.info[brewer.pal.info$category == 'qual',]
 col_vector <- unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
 library(car)
-scatter3d(D[, 1], D[, 2], D[, 3], point.col = col_vector[partition(D, L, C)], surface = FALSE)
-plot(D[, 2], partition(D, L, C))
+scatter3d(D[, 1], D[, 2], D[, 3], point.col = col_vector[partSEB(D, L, C)], surface = FALSE)
+# plot(D[, 2], partSEB(D, L, C))
 
